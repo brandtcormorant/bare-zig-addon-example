408c1dab0f34c85deaf6be3e89b3ce5b0811bc1f
brandtcormorant <brandtcormorant@proton.me>
2024-12-27 20:44:56 -0800

    remove std

--- src/c_api.zig
+++ src/c_api.zig
@@ -1,7 +1,3 @@
-const std = @import("std");
-const example = @import("root.zig");
-
 export fn bare_zig_add(a: f64, b: f64) callconv(.C) f64 {
-    std.debug.print("from zig c api export: n1: {}, n2: {}\n", .{ a, b });
-    return example.add(a, b);
+    return a + b;
 }
