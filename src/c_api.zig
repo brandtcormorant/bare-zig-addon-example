dcf409168d22e5cfc6b16461dcfad9b9f386df0c
brandtcormorant <brandtcormorant@proton.me>
2024-12-27 22:22:19 -0800

    let the print flow

--- src/c_api.zig
+++ src/c_api.zig
@@ -1,3 +1,7 @@
+const std = @import("std");
+
 export fn bare_zig_add(a: f64, b: f64) f64 {
+    const sum = a + b;
+    std.debug.print("from zig: n1: {} + n2 {} = {}", .{a, b, sum});
     return a + b;
 }
